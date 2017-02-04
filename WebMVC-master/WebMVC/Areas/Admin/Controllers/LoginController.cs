9ae2002ac580b8846bc98560d6d9c61463b9e12d
h0cc0i <bk.hoccoi@gmail.com>
2017-02-04 21:13:41 +0900

    update before change template

--- WebMVC-master/WebMVC/Areas/Admin/Controllers/LoginController.cs
+++ WebMVC-master/WebMVC/Areas/Admin/Controllers/LoginController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 using WebMVC.COMMON;
 using WebMVC.DataAccessLayer;
 using WebMVC.ENTITIES.CrudModel;
@@ -40,6 +41,13 @@ namespace WebMVC.Areas.Admin.Controllers
                 }
             }
                 return View("Index");
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            Session.Abandon();
+            return View("Index","Home");
         }
     }
 
